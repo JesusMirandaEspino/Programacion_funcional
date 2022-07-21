01135d022628e38a941ebd3e88b68f59c5712df3
José de Jesús Miranda Espino <51500381+JesusMirandaEspino@users.noreply.github.com>
2022-07-20 19:10:03 -0500

    semana3 ejercicio2

---
+++ evaluacion3/ejercicio2.py
@@ -0,0 +1,20 @@
+
+lista_productos = ( 'Huevo', 'frijol', 'Arroz', 'Leche', 'Tortilla', 'Pollo', 'Frutas', 'Verduras', 'Avena', 'Cereal')
+lista_precio = ( 40.5, 20.5, 30.5, 20, 20, 32.5, 22.5 , 18.5, 15, 45.5 )
+
+nuevo_precio = lambda precio : precio * 0.16
+
+def agregar_iva(lista_precio):
+    nuevos_precios = map(nuevo_precio, lista_precio)
+    return list(nuevos_precios)
+
+
+lista_nuevos_precio = agregar_iva(lista_precio)
+
+
+def imprimir_productos(productos, precio):
+    for index, producto in enumerate(productos):
+        print("Producto #{} = {}, Precio: {}".format(index, producto, precio[index]))
+
+
+imprimir_productos(lista_productos, lista_nuevos_precio)
